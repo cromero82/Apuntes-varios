24d6f5e56f1283764cd8d85bc85af0822cf8a607
Carlos Romero <soportecarlosromero@gmail.com>
2019-07-10 22:59:57 -0500

    Create modificarColumna.sql

---
+++ Bases de Datos/SqlServer/modificarColumna.sql
@@ -0,0 +1,5 @@
+------------------------------------------------------
+-- Tambien es posible redimensionar o cambiar tipos, o 
+-- la propiedad null.
+------------------------------------------------------
+alter table FinCategoriaElemento alter column  nombre varchar(50) not null;
