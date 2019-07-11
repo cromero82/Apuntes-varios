8ebd03f04c40e4c38c9d604096df567f8943f81a
Carlos Romero <soportecarlosromero@gmail.com>
2019-07-10 23:08:34 -0500

    Create alterarConstraint.sql

---
+++ Bases de Datos/SqlServer/alterarConstraint.sql
@@ -0,0 +1,5 @@
+--alter table FinProducto drop column Estregistro;
+--alter table FinProducto drop CONSTRAINT UNIQ_PROD;
+alter table FinProducto add CONSTRAINT FK_FinElementos_FinProducto FOREIGN KEY (elementoId) REFERENCES FinElementos (id);
+alter table FinProducto add CONSTRAINT FK_FinMarca_FinProducto FOREIGN KEY (marcaId) REFERENCES FinMarcas (id);
+GO
