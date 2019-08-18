7c779ea87e1fff98ee67ccb0f9674a12d5e874a2
Carlos Romero <soportecarlosromero@gmail.com>
2019-08-17 22:14:39 -0500

    acceso a la consola CloudShell

--- Google Cloud/cloudShell.ts
+++ Google Cloud/cloudShell.ts
@@ -1,4 +1,4 @@
-// Cloud shell, Permite ejecutar comendos en nuestros proyectos GoogleCloud
+// Cloud shell, Permite ejecutar comendos en nuestros proyectos GoogleCloud, se accede con click en la parte superior derecha
 
 // lista los proyectos que hemos creado (máximo 5 por cuenta)
 gcloud projects list
