70e94f25e0fd33b2e17098d223de969d5b62ceb4
Carlos Romero <soportecarlosromero@gmail.com>
2019-09-11 09:52:33 -0500

    Update PrimerRegistro.sql

--- Bases de Datos/PrimerRegistro.sql
+++ Bases de Datos/PrimerRegistro.sql
@@ -1,1 +1,5 @@
+-- For oracle 11G-
 select hfmi.estado from  HCL_FORMMEDINSUMOS hfmi where hfmi.formmedicas_id = hfm.id FETCH FIRST 1 ROWS ONLY
+
+-- For oracle 12C+
+select * from lista_item where ID>0 AND ROWNUM <= 1
