c866e9b1d4277bb0026bff4fac88a8706918bbbe
Carlos Romero <soportecarlosromero@gmail.com>
2019-10-18 14:23:18 -0500

    Create truncarEsquema.sql

---
+++ Bases de Datos/Oracle/truncarEsquema.sql
@@ -0,0 +1,33 @@
+CREATE OR REPLACE PROCEDURE SP_DELETE_ALL AS 
+BEGIN
+  declare
+
+v_str1 varchar2(200) := null;
+
+cursor get_sql is
+select
+     'drop '||object_type||' '|| object_name|| DECODE(OBJECT_TYPE,'TABLE',' CASCADE CONSTRAINTS PURGE') v_str1
+from user_objects
+where object_type in ('TABLE','VIEW','PACKAGE','TYPE','FUNCTION','TRIGGER','SEQUENCE')
+order by object_type,object_name;
+
+begin
+
+open get_sql;
+
+loop
+
+fetch get_sql into v_str1;
+
+          if get_sql%notfound
+          then exit;
+          end if;
+
+execute immediate v_str1;
+
+end loop;
+
+close get_sql;
+
+end;
+END SP_DELETE_ALL;
