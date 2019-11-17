0c0c85c47e7e9bbc3d866342e13fd3d1e55ac52d
Carlos Romero <soportecarlosromero@gmail.com>
2019-11-17 07:10:09 -0500

    Create InstalacionCreacionProjecto.js

---
+++ ionic/InstalacionCreacionProjecto.js
@@ -0,0 +1,11 @@
+// ======================  Instalación de Ionic ======================
+// 1. Instalar NodeJs
+// 2. Desde cualquier ruta
+>npm install -g ionic
+
+// ======================  Creación del proyecto Ionic ======================
+// 1.  Ir a la carpeta del proyecto y en terminal
+> ionic start nombreApp tipoApp
+// donde: tipoAppp: task, blank, sidemenu
+// 2. Ejecutar la solución
+> ionic serve -o
