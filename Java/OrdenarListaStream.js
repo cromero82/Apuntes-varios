25b38028e901a7cb13db40b75e492e27de776681
Carlos Romero <soportecarlosromero@gmail.com>
2019-12-02 18:24:56 -0500

    Create OrdenarListaStream.js

---
+++ Java/OrdenarListaStream.js
@@ -0,0 +1,4 @@
+//				List<DiagnosticoFalla> fallasOrdenadas = fallaMod.stream().sorted((o1, o2)->o2.getId().
+//                        compareTo(o1.getId())).
+//                        collect(Collectors.toList());
+//				fallaMod = fallasOrdenadas;
