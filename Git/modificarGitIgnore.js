31f477a4ccf7e7ee11bb884ef1f1a3f7b4c7f7f8
Carlos Romero <soportecarlosromero@gmail.com>
2020-02-12 15:58:54 -0500

    Create modificarGitIgnore.js

---
+++ Git/modificarGitIgnore.js
@@ -0,0 +1,5 @@
+1. Editar el archivo git.ignore agregando directororios y extensiones de archivos que se desean no esten versionados
+2. Ejecutar consola del proyecto git
+3. git config --global core.excludesfile ~/.gitignore_global
+4. crear un commit respectivo ya que se ha editado 1 archivo (git.ignore)
+5. push a la rama (ojala master o develo)
