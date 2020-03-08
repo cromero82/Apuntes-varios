e44fd2e46f455869523f37073a05f5944467daa8
Carlos Romero <soportecarlosromero@gmail.com>
2020-03-08 17:22:14 -0500

    Create comandos básicos.js

---
+++ NetCoreVSCode/comandos básicos.js
@@ -0,0 +1,8 @@
+// Creacion de un proyecto webApi en la ruta actual
+dotnet new webApp -o aspnetcoreapp
+
+// compilación de un proyecto, debo ubicarme en cada proyecto para compilarlo
+dotnet build 
+
+// ejecuta un proyecto
+dotnet run
