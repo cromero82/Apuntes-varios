72475358e7d4c18ea49847efce9bd13cec31a544
Carlos Romero <soportecarlosromero@gmail.com>
2020-03-16 21:12:06 -0500

    Create instalarNetCore.cs

---
+++ linux/instalarNetCore.cs
@@ -0,0 +1,8 @@
+// Instalar .NET CORE 3.1 Runtime 
+$ sudo apt-get update
+$ sudo apt-get install apt-transport-https
+$ sudo apt-get update
+$ sudo apt-get install aspnetcore-runtime-3.1
+
+// Comprobar
+$ dotnet --version
