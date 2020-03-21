e3d6d64f9c3fec8aa181c3281b0b0c3e9157c424
Carlos Romero <soportecarlosromero@gmail.com>
2020-03-21 14:00:46 -0500

    Create directorios_archivos.js

---
+++ linux/directorios_archivos.js
@@ -0,0 +1,23 @@
+// superadministrador
+$ sudo su
+
+// crear un directorio
+$ mkdir nombre_dir
+
+// borrar un directorio
+$ rmdir nombre_dir
+
+// Permisos de lectura y escritura de directorios
+$ chmod 777 ruta_directorio
+
+// =======================          NANO TEXT EDITOR          ======================================
+// abrir un archivo
+$ nano /etc/xxxxx/ruta_directorio/nombre_archivo.ext
+
+// crear y abrir un archivo
+$ nano /etc/xxxxx/ruta_directorio/nombre_archivo.ext
+
+// en cualquier caso guardar cambios
+// luego de editar un archivo presionamos: Ctrl + X
+// presionamos: Enter (confirmando el nombre de archivo) o la opcion Y: (Yes save changes)
+
