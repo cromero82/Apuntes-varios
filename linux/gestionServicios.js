136de33c9b7c177447d1513cf7c47b1d9b49335f
Carlos Romero <soportecarlosromero@gmail.com>
2020-03-21 18:55:43 -0500

    Create gestionServicios.js

---
+++ linux/gestionServicios.js
@@ -0,0 +1,20 @@
+// =============        COMANDO service       ===============
+// -----------------------------------------------------------
+// reiniciar servicio
+$ service nginx restart
+// iniciar servicio
+$ service nginx status
+
+// detener servicio
+$ service nginx stop
+
+// iniciar servicio
+$ service nginx start
+
+// ==============       COMANDO systemctl     ===============
+// -----------------------------------------------------------
+// iniciar servicio
+$ systemctl start nginx.service
+
+// estado de un servicio
+$ systemctl status nginx.service
