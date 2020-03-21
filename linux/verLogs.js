32014d62e077e9565461ad86153ad1c267a17e1b
Carlos Romero <soportecarlosromero@gmail.com>
2020-03-21 18:51:43 -0500

    Create verLogs.js

---
+++ linux/verLogs.js
@@ -0,0 +1,6 @@
+// referencia para ver logs
+// aqui una guia completa: https://www.digitalocean.com/community/tutorials/how-to-use-journalctl-to-view-and-manipulate-systemd-logs
+$ journalctl
+
+// ver logs de un servicio en particular
+$ journalctl -u nginx.service
