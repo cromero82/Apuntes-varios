1bef30629aeec2728182157700dbf5d136c3091e
Carlos Romero <soportecarlosromero@gmail.com>
2020-03-22 14:33:19 -0500

    descomprimir archivos .zip

---
+++ linux/zip-unzip.js
@@ -0,0 +1,7 @@
+// -------------- descomprimir archivos
+// descromprime un archivo .zip que se encuentra en la ruta /var/www-nginx llamado unapp.zip, creando una archivo en la misma ruta
+$ unzip /var/www-nginx/unapp.zip -d /var/www-nginx
+
+// --------------- otros comandos importantes
+// eliminar directorios con subdirectorios
+$ rm -r /var/www-nginx/unapp
