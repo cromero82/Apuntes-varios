370041019d01c1cf97c060a87ee660f18b955819
Carlos Romero <soportecarlosromero@gmail.com>
2020-04-10 17:06:14 -0500

    Create crearUsuarioConTodosPrivileg.sql

---
+++ Bases de Datos/Oracle/crearUsuarioConTodosPrivileg.sql
@@ -0,0 +1,6 @@
+-- usuario root en Oracle = system
+-- 1. Ingresar con este usuario y luego definir usuario y password.
+
+CREATE USER CARLOS IDENTIFIED BY C4RL0S; 
+
+GRANT ALL PRIVILEGES TO CARLOS; 
