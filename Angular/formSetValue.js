89a594375551cb87514035fbe237f7b16258e58a
Carlos Romero <soportecarlosromero@gmail.com>
2020-04-22 10:24:17 -0500

    aclaracion para deshabilitar Select

--- Angular/formSetValue.js
+++ Angular/formSetValue.js
@@ -1,3 +1,7 @@
+// Cuando el select no tiene asignado un elemento formControl se puede realizar mediante una variable
+// [disabled] = "booleanVar" en el html
+// pero de lo contrario se debe usar dicho elemento para controlarlo
+
 // Finalmente no me quedo tan claro cual funciona
 this.formularioTransicion.get('responsable').value(null);
 this.formularioTransicion.controls['responsable'].setValue(null);
