c2963ff0bca44ebad2cc482062802a59856afb55
Carlos Romero <soportecarlosromero@gmail.com>
2020-05-11 20:39:25 -0500

    Create comandosVim.js

---
+++ linux/comandosVim.js
@@ -0,0 +1,19 @@
+Editor VIM
+------------------------------------------
+Comandos básicos:
+Habilitar modo de edición:		Press: i
+Salir y guardar cambios:		Press: esc + : +  x
+Salir SIN guardar cambios:		Press: esc + : +  q! 
+
+
+Problemas de Encriptar - Quitar Encriptación
+Cómo agregar encriptación.
+	Al momento de salir y guardar cambios, utilizamos la combinacion: Press: esc + : +  x
+	pero si nos equivocamos y utilizamos el caracter ‘X’ (Es decir en mayuscula) entonces se habilita 
+la encriptación, aparece un cuadro de diálogo informando que va aplicar encriptación al archivo y solicita una contraseña. tenemos dos opciones:
+i) Presionar dos veces enter y evitar asi aplicar esta encriptación.
+ii) si colocamos un password el archivo sera guardado con la encriptación
+
+Como quitar encriptación
+Si el archivo esta encriptado debemos abrirlo nuevamente, y al momento de guardarlo: Press: esc + : +  X  
+(utilizando X mayuscul) cuando solicite la contraseña de encriptación presionamos enter (Es decir dos veces enter).
