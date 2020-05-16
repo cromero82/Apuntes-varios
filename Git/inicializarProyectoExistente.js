cda0d36da6b67006ad10ce8e81d208aaf6839174
Carlos Romero <soportecarlosromero@gmail.com>
2020-05-16 11:03:39 -0500

    Create inicializarProyectoExistente.js
    Escenario: Se tiene un repositorio creado en github un proyecto y en local se tiene un proyecto  (sin estar versionado, es decir sin el origin) y se requiere que los cambios locales se versionen (se pasen al respositorio en github.com)

---
+++ Git/inicializarProyectoExistente.js
@@ -0,0 +1,8 @@
+// Escenario: Se tiene un repositorio creado en github y se tiene un proyecto creado en local 
+// (sin estar versionado, es decir sin el origin) y se requiere que los cambios locales se versionen
+
+1. > git remote add origin aqui_ruta_repositorio_en_git
+2. > git add .
+3. > git commit -m "creacion inicial desde local"
+4. > git pull origin master
+5. > git push -f origin master
