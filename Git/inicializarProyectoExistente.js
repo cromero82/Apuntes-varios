2ac1a1ec97009e58f8de4191b5b30a6b8f6b2fda
Carlos Romero <soportecarlosromero@gmail.com>
2020-05-17 13:49:47 -0500

    Update inicializarProyectoExistente.js
    fix inicializar directorio

--- Git/inicializarProyectoExistente.js
+++ Git/inicializarProyectoExistente.js
@@ -1,6 +1,6 @@
 // Escenario: Se tiene un repositorio creado en github y se tiene un proyecto creado en local 
 // (sin estar versionado, es decir sin el origin) y se requiere que los cambios locales se versionen
-
+*. > git init
 1. > git remote add origin aqui_ruta_repositorio_en_git
 2. > git add .
 3. > git commit -m "creacion inicial desde local"
