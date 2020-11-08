53f66098b1ccc9c12005c35636f2acf669f170fb
Carlos Romero <soportecarlosromero@gmail.com>
2020-11-07 23:58:40 -0500

    Update tips.js

--- Teclado/tips.js
+++ Teclado/tips.js
@@ -1,7 +1,31 @@
 1. escritorios virtuales
   Crear: Windows + Ctrl + D
   Navegar: Windows + Ctrl + (Arrows)
-
+  Verlos (Administrarlos) Windows + Tab
 2. Capturas de pantalla y guardado automático
   Windows + Impr Pant Petsis
-  
+
+--------------------------  
+---    Navegador
+--------------------------  
+3. Navegar entre tabs
+  Ctrl + Numero
+
+4. Nueva ventana. Ctrl + N
+   Nueva Pestaña. Ctrl + T
+
+5. Borrar Tab. Ctrl + W
+               Ctrl + F4
+
+6. Abrir Pestaña recientemente creada: Ctrl + Shift + T
+
+7. Ir a la barra de direcciones: Shift + L
+8. Ir a descargas: Ctrl + j
+9. Navegar entre campos de formulario hacia atras: Shift + Tab
+10. Volver atras. Alt + Flechas
+11. Dividir pantalla. Windows + (flechas)
+
+Sitios Web.
+https://www.nitro-pc.es/blog/atajos-de-teclado/
+https://www.adslzone.net/como-se-hace/chrome/atajos-teclado-chrome/
+https://logico.ar/blog/2019/01/29/una-vida-sin-mouse-o-como-aprendi-a-utilizar-el-teclado-para-todo
