6fca9ae31d989557ef826b49935c7440d6164616
Carlos Romero <soportecarlosromero@gmail.com>
2021-02-02 16:59:49 -0500

    git stash con archivos nuevos y nombre

--- Git/gitStash.js
+++ Git/gitStash.js
@@ -1,3 +1,6 @@
+//----- Guarda un stash con archivos nuevos y con un nombre personalizado
+ git stash save "cambios temporales de RQ1570" -u
+
 //----- Guarda en un stash inclusive con archivos nuevos
 git stash -u
 
@@ -5,10 +8,10 @@ git stash -u
 git stash save "intellij files".
 
 //----- Listar  stash guardados
-	$ git stash list
+$ git stash list
 		stash@{0}: On GP2884: intellij files
 		stash@{1}: WIP on RQ1246: 58a3f129 CORRECCION MANTIS: 0001292 ERROR NOMENCLATURA DE FIRMA V1.5
 		stash@{2}: WIP on master: 936aa969 Merge branch 'hotfix/GP2876' into master 1096ee7
 		
 //----- Aplicar un stash en particular
-		git stash apply stash@{0}
+git stash apply stash@{0}
