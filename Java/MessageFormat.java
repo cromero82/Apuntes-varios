6338fe808e5f45e94a28fe63f007a8b86f6525b9
Carlos Romero <soportecarlosromero@gmail.com>
2021-05-31 16:58:03 -0500

    String con parametros {n}

---
+++ Java/MessageFormat.java
@@ -0,0 +1,2 @@
+ String parametrosEntrada = new MessageFormat("numeroDocumento: {0}, idProyecto:{1} numeroEncargo: {2}, fideicomiso: {3}")
+        .format(tercero.getNumeroDocumento(), idProyecto,numeroEncargo, codigoFideicomiso );
